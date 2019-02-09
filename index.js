53705166136fecf863908d63844d8d113a784dfc
Henrique Kuwai <eu@henriquekuwai.com.br>
2019-02-09 16:26:33 -0200

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const package = require('./package.json');
+
+program.version(package.version);
+
+program
+    .command('add <todo>')
+    .description('Adiciona um to-do')
+    .action((todo) => {
+        console.log(todo);
+    });
+
+program.parse(process.argv);
